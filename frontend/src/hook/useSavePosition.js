c96c53c8bc96cdf54e23fd6f1305fca76d6ab4bc
HarunHatiboglu <harun.hatiboglu@helloworld.com.tr>
2023-08-07 18:52:28 +0300

    add commit vercel deploy

--- frontend/src/hook/useSavePosition.js
+++ frontend/src/hook/useSavePosition.js
@@ -4,11 +4,14 @@ import axios from "axios";
 export function useSavePosition(position, setMarkers) {
   return useCallback(async () => {
     try {
-      const response = await axios.post("http://localhost:4000/api/points", {
-        id: Math.random(),
-        lat: position.lat.toFixed(2),
-        lng: position.lng.toFixed(2),
-      });
+      const response = await axios.post(
+        "https://map-sand.vercel.app/api/points",
+        {
+          id: Math.random(),
+          lat: position.lat.toFixed(2),
+          lng: position.lng.toFixed(2),
+        }
+      );
       console.log("Position saved:", response.data);
       setMarkers((prevMarkers) => [...prevMarkers, response.data]);
     } catch (error) {
